c111decfbf0c66047e3153fa9a2c0b63545de8db
EgemenUzun <uzun.aegemen@gmail.com>
2023-05-07 14:30:30 +0300

    Ai

--- Assets/Scripts/Enemies/HitFeedBack.cs
+++ Assets/Scripts/Enemies/HitFeedBack.cs
@@ -4,32 +4,51 @@ using UnityEngine;
 
 public class HitFeedBack : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public static float EnemyHealth;
+    string getEnemyName;
+    EnemyAI EAI;
+    EnemyAIRanged EAIR;
     private Animator animator;
-    [SerializeField]
     private Animator playerAnimator;
     void Start()
     {
         playerAnimator = GameObject.Find("Player").GetComponent<Animator>();
+        animator = GetComponent<Animator>();
+        EAI = GetComponent<EnemyAI>();
+        EAIR = GetComponent<EnemyAIRanged>();
+        getEnemyName = gameObject.tag;
+        if (getEnemyName == "Troll")
+            EnemyHealth = 200f;
+        else if (getEnemyName == "Skeleton")
+            EnemyHealth = 100f;
+        else if (getEnemyName == "DemonGirl")
+            EnemyHealth = 150f;
+        else if (getEnemyName == "Wizard")
+            EnemyHealth = 500f;
     }
 
     // Update is called once per frame
     void Update()
-    { 
-
-    }
-    private void Awake()
     {
-        animator = GetComponent<Animator>();
+        print(EnemyHealth);
     }
     private void OnTriggerEnter(Collider other)
     {
-        
         if (other.CompareTag("Sword") && playerAnimator.GetBool("combo"))
         {
-           
             animator.SetTrigger("GetHit");
-            
+            EnemyHealth -= 15f;
+            Diead();
+        }
+    }
+    void Diead()
+    {
+        if(EnemyHealth <=0)
+        {
+            animator.SetTrigger("Die");
+            Destroy(EAI);
+            Destroy(EAIR);
+            Destroy(gameObject ,4f);
         }
     }
 
