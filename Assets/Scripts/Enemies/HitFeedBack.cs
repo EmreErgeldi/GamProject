081e1bcb4d34f5152e59daf80ef563f9a3c7058a
EgemenUzun <uzun.aegemen@gmail.com>
2023-05-10 23:50:17 +0300

    AI fix

--- Assets/Scripts/Enemies/HitFeedBack.cs
+++ Assets/Scripts/Enemies/HitFeedBack.cs
@@ -32,7 +32,7 @@ public class HitFeedBack : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       print(EnemyHealth);
+       //print(EnemyHealth);
     }
     private void OnTriggerEnter(Collider other)
     {
